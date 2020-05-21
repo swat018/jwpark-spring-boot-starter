2eab63a83138fb46269e6b9c20b9bdf9f89742ba
jinwoo park <swat018@gmail.com>
2020-05-21 23:39:03 +0900

    Initial commit

---
+++ src/main/java/com/swat018/HolomanConfiguration.java
@@ -0,0 +1,18 @@
+package com.swat018;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HolomanConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Holoman holoman() {
+        Holoman holoman = new Holoman();
+        holoman.setHowLong(5);
+        holoman.setName("jinwoo");
+        return holoman;
+    }
+}
